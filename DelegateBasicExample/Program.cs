b73e86bd26ed98401bcf197b7e6d29e641eafb6c
Abdul Kader <mak.sabuj@hotmail.com>
2023-12-13 13:41:24 +0600

    delegates done

---
+++ DelegateBasicExample/Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class Program
+{
+    private delegate void LogDel(string text);
+    private static void Main(string[] args)
+    {
+        var log = new Log();
+        
+        
+        
+        var logDel = new LogDel(log.LogTextToScreen);
+        
+        Console.WriteLine("Please enter your name: ");
+        var name = Console.ReadLine();
+        // logDel.Invoke(name!);
+
+        var logText = new LogDel(log.LogTextToFile);
+        // logText.Invoke(name!);
+
+        var multiLogDel = logDel + logText;
+        // multiLogDel(name!); 
+        LogText(logText, name!);
+    }
+
+    static void LogText(LogDel logDel, string text)
+    {
+        logDel(text);
+    }
+
+   
+}
+
+public class Log
+{
+    public void LogTextToScreen(string text)
+    {
+        Console.WriteLine($"{DateTime.Now}: {text}");
+    }
+
+
+    public void LogTextToFile(string text)
+    {
+        using var sw = new StreamWriter(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Log.txt"), true);
+        sw.WriteLine($"{DateTime.Now}: {text}");
+    }
+}
